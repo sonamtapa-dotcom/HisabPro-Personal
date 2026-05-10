d6c4d4e01f8f1172e98ba39c429c07669474e5e8
sonamtapa-dotcom <sonamtapa@gmail.com>
2026-05-10 10:18:39 +0545

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='hisabpro-v1-5-real-app';
+const CACHE='hisabpro-v1-6';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))));self.clients.claim();});
